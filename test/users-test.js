6d47e5a381a6eb257f98276ffe1db5d4591c4cd4
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-01 17:11:15 -0800

    Add import for users test

---
+++ test/users-test.js
@@ -0,0 +1,11 @@
+import chai from "chai";
+const expect = chai.expect;
+import User from "./classes/user.js"
+
+describe("User", () => {
+    let user;
+
+    beforeEach(()=> {
+        user = new User()
+    })
+})
