b3604b85b094e6fa580677e9ace6799165d19a32
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-02 12:56:27 -0800

    Updata data to match users id

---
+++ data/booking-data.js
@@ -0,0 +1,21 @@
+const sampleBookingData = [
+  {
+    id: "5fwrgu4i7k55hl6t8",
+    userId: 1,
+    data: "2022/02/05",
+    roomNumber: 12,
+  },
+  {
+    id: "5fwrgu4i7k55hl6sz",
+    userId: 9,
+    data: "2022/04/22",
+    roomNumber: 15,
+  },
+  {
+    id: "5fwrgu4i7k55hl6tl",
+    userId: 3,
+    data: "2022/01/10",
+    roomNumber: 8,
+  },
+];
+export default sampleBookingData
