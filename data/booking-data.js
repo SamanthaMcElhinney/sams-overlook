3a394f7a040fd1edb90d2923fb8f67fbad6fa478
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-03 15:19:00 -0800

    update sample data for bookings

--- data/booking-data.js
+++ data/booking-data.js
@@ -1,19 +1,19 @@
 const sampleBookingData = [
   {
     id: "5fwrgu4i7k55hl6t8",
-    userId: 1,
+    userID: 1,
     data: "2022/02/05",
     roomNumber: 12,
   },
   {
     id: "5fwrgu4i7k55hl6sz",
-    userId: 9,
+    userID: 9,
     data: "2022/04/22",
     roomNumber: 15,
   },
   {
     id: "5fwrgu4i7k55hl6tl",
-    userId: 3,
+    userID: 3,
     data: "2022/01/10",
     roomNumber: 8,
   },
