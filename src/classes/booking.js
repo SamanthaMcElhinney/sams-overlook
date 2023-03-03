75288893e0a42153faf736ce1a4ab6f59c05be33
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-03 15:20:05 -0800

    Update booking naming to match API data

--- src/classes/booking.js
+++ src/classes/booking.js
@@ -3,12 +3,12 @@ import Room from "./room.js";
 class Booking {
   constructor(bookingData) {
     this.id = bookingData.id || null;
-    this.userId = bookingData.userId;
+    this.userID = bookingData.userID;
     this.date = bookingData.date;
     this.roomNumber = bookingData.roomNumber;
   }
   findRoom(roomData){
-    const foundRoom = roomData.find(room => room.number === this.number)
+    const foundRoom = roomData.find(room => room.number === this.roomNumber)
     return new Room(foundRoom)
   }
 
