ec465e7871f23ab30b4e4a676a2439209c879df0
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-04 23:09:57 -0800

    Update css styles to include table

--- src/classes/hotel.js
+++ src/classes/hotel.js
@@ -8,42 +8,59 @@ class Hotel {
     }
 
     filterByDate(date) {
-        let selectedDate = this.bookings.filter(booking => booking.date === date)
-        console.log(selectedDate, "SD")
-        let bookingOnAvailableDate = selectedDate.reduce((rooms, booking) => {
-            console.log(booking, "booking")
-            let isBooked = false
-            this.rooms.forEach((room) => {
-                if (room.number !== booking.roomNumber) {
-                    rooms.push(booking)
-                } else if (room.number === booking.number) {
-                    isBooked = true
-                }
-            })
-            console.log(rooms, "rooms")
-            return rooms
-        }, [])
-        console.log(bookingOnAvailableDate, "BOOKING ON AVAILABLE DATE")
-        return bookingOnAvailableDate;
+            // let selectedDate = this.bookings.filter(
+            //   (booking) => booking.date === date
+            // );
+            // let bookingOnAvailableDate = this.rooms.reduce((rooms, booking) => {
+            //   let isBooked = false;
+            //   this.rooms.forEach((room) => {
+            //     if (room.number !== booking.roomNumber) {
+            //       rooms.push(booking);
+            //     } else if (room.number === booking.number) {
+            //       isBooked = true;
+            //     }
+            //   });
+            //   return rooms;
+            // }, []);
+            // console.log(bookingOnAvailableDate, "BOOKING ON AVAILABLE DATE");
+            // return bookingOnAvailableDate;
+        const bookingsOnSelectedDate = this.bookings.filter(booking => booking.date === date);
+        const filteredAvailableRooms = this.rooms.filter(room => {
+            return bookingsOnSelectedDate.every(booking => {
+                return booking.roomNumber !== room.number;
+            });
+        });
+        if (!filteredAvailableRooms.length) {
+            return;
+        } else {
+            return filteredAvailableRooms;
+        }
     }
+
 
     filterByRoomType(roomType, date) {
         let hotelStatus = "available"
-        let availableRoomsByDate = this.filterByDate(date)
-        let availableFilteredRooms = availableRoomsByDate.filter(room =>
+        const availableRoomsByDate = this.filterByDate(date)
+        console.log(availableRoomsByDate, "available rooms by date")
+        const availableFilteredRooms = availableRoomsByDate.filter(room =>
             room.roomType === roomType)
-        if (!availableRoomsByDate) {
-            hotelStatus = "unavailable"
+        if (!availableFilteredRooms.length) {
+            return hotelStatus = "unavailable"
         } else {
-            console.log(availableFilteredRooms, "filteredROOM AVAIL")
-            return availableFilteredRooms
+            console.log(availableFilteredRooms, "filteredROOM AVAIL");
+            return availableFilteredRooms;
         }
-
     }
 
-    // addBooking(data, user, selectedRoom){
+    addBooking(date, user, selectedRoom) {
+        this.bookings.push(new Booking({
+            "id": Date.now(),
+            "userID": user.id,
+            "date": date,
+            "roomNumber": selectedRoom.number
+        }, this.rooms))
+    }
 
-    // }
 }
 
 export default Hotel
